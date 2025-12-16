63219d36446368c42b41b3c3a9cf405aa0d05f39
Tetsuo Honda <t-honda@hondarer-soft.com>
2025-12-17 08:05:33 +0900

    PATH順番変更

--- .github/workflows/Add-VSBT-Env-x64.ps1
+++ .github/workflows/Add-VSBT-Env-x64.ps1
@@ -37,7 +37,9 @@ if (-not (Test-Path $sdkBin)) {
 }
 
 # GitHub Actions の環境変数に PATH を追加
-$pathsToAdd = @($msvcBin, $sdkBin, $sdkUcrtBin, $diaBin)
+# 注: GitHub Actions では後に追加したパスほど PATH の先頭に来るため、
+#     MSVC のパスを最後に追加することで、link.exe などが確実に MSVC のものになる
+$pathsToAdd = @($diaBin, $sdkUcrtBin, $sdkBin, $msvcBin)
 
 foreach ($pathToAdd in $pathsToAdd) {
     Write-Host "Adding to PATH: $pathToAdd"
--- prod/testmain/testmain.c
+++ prod/testmain/testmain.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#if _WIN32
+#pragma warning(push)
+#pragma warning(disable: 4100)
+#else
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#endif
+
 int main(int argc, char *argv[])
 {
     printf("Hello, World\n");
     return 0;
 }
+
+#if _WIN32
+#pragma warning(pop)
+#else
+#pragma GCC diagnostic pop
+#endif
