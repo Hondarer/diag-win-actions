ba007dabea97e70b271f62dcec9dd27572d89bc2
Tetsuo Honda <t-honda@hondarer-soft.com>
2025-12-19 07:53:52 +0900

    不足ファイル追加

---
+++ test/makeflags.mk
@@ -0,0 +1,90 @@
+ifneq ($(OS),Windows_NT)
+    # Linux
+    CFLAGS=\
+		-Wall \
+		-Wextra \
+		-Wcast-align \
+		-Wdisabled-optimization \
+		-Wfloat-equal \
+		-Wformat=2 \
+		-Winit-self \
+		-Winline \
+		-Winvalid-pch \
+		-Wmissing-format-attribute \
+		-Wmissing-noreturn \
+		-Wpacked \
+		-Wpadded \
+		-Wredundant-decls \
+		-Wstrict-aliasing=2 \
+		-Wswitch-default \
+		-Wswitch-enum \
+		-Wundef \
+		-Wvariadic-macros \
+		-Wshadow \
+		-Wmissing-declarations \
+		-Wmissing-prototypes \
+		-Wstrict-prototypes \
+		-Wsequence-point \
+		-Wpointer-arith \
+		-Wbad-function-cast \
+		-Wunsafe-loop-optimizations \
+		-Wnested-externs \
+		-Wcast-qual \
+		-Wint-to-pointer-cast \
+		-Wpointer-to-int-cast \
+		-Wimplicit-fallthrough=0 \
+		-Wformat-overflow=0 \
+		-Wunknown-pragmas \
+		-W
+    CXXFLAGS=\
+		-Wall \
+		-Wextra \
+		-Wcast-align \
+		-Wdisabled-optimization \
+		-Wfloat-equal \
+		-Wformat=2 \
+		-Winit-self \
+		-Winline \
+		-Winvalid-pch \
+		-Wmissing-format-attribute \
+		-Wmissing-noreturn \
+		-Wpacked \
+		-Wpadded \
+		-Wredundant-decls \
+		-Wstrict-aliasing=2 \
+		-Wswitch-default \
+		-Wswitch-enum \
+		-Wundef \
+		-Wvariadic-macros \
+		-Wshadow \
+		-Wmissing-declarations \
+		-Wsequence-point \
+		-Wpointer-arith \
+		-Wunsafe-loop-optimizations \
+		-Wcast-qual \
+		-Wint-to-pointer-cast \
+		-Wimplicit-fallthrough=0 \
+		-Wformat-overflow=0 \
+		-Wunknown-pragmas \
+		-W
+    LDFLAGS     =
+else
+    # Windows
+    CFLAGS      =
+    CXXFLAGS    =
+    LDFLAGS     =
+endif
+
+LIBSDIR    +=\
+	$(WORKSPACE_FOLDER)/testfw/lib \
+	$(WORKSPACE_FOLDER)/test/lib
+
+# テストフレームワークをリンクする
+LINK_TEST = 1
+
+# テスト関連ライブラリは、すべて静的リンクとする
+ifeq ($(OS),Windows_NT)
+    # Windows
+    CFLAGS   += /DCALC_STATIC
+    CXXFLAGS += /DCALC_STATIC
+endif
